fa202a07adec6ec80bbe682f79d00cb66c98150b
Robin Knipe <robin.knipe@gmail.com>
2019-08-22 11:57:14 +0100

    Clean up LevReport code and add Group Usage grid
     - get rid of some of the test/debug code
     - drop the unhelpful GOV.UK `Main` component
     - handle datasets itteratively (for variably many)
     - add the Group Usage grid component to the report page

--- src/lev-report/index.js
+++ src/lev-report/index.js
@@ -1,38 +1,21 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Main } from 'govuk-react';
 import LevPage from '../lev-page';
-import ReportFilters from "./ReportFilters";
-import LevUsage from "./charts/LevUsage";
-const moment = require('moment');
-
-const SHORT_FORMAT = 'D/M/YYYY';
-
-const rand = n => Math.round(Math.random() * n);
-
-const FullWidthMain = styled(Main)`
-  & > div > div {
-    max-width: 100%;
-    width: 100%;
-  }
-`;
+import ReportFilters from './ReportFilters';
+import LevUsage from './charts/LevUsage';
+import UsageByGroup from "./grid/UsageByGroup";
 
 class LevReport extends React.Component {
   constructor(props) {
       super(props);
       this.state = {
-        from: '',
-        to: '',
+        from: props.from,
+        to: props.to,
         values: '',
-        birth: props.birth,
-        death: props.death,
-        marriage: props.marriage,
-        partnership: props.partnership,
         dates: props.dates,
-        totalBirths: props.birth.reduce((t, n) => t + n.usage, 0),
-        totalDeaths: props.death.reduce((t, n) => t + n.usage, 0),
-        totalMarriages: props.marriage.reduce((t, n) => t + n.usage, 0),
-        totalPartnerships: props.partnership.reduce((t, n) => t + n.usage, 0)
+        datasets: props.datasets,
+        dailyUsage: props.dailyUsage,
+        groups: props.groups,
+        totals: props.totals
       };
   }
 
@@ -45,37 +28,9 @@ class LevReport extends React.Component {
   }
 
   handleSubmit(e) {
-    e.preventDefault();
-    const from = moment(this.state.from).format(SHORT_FORMAT);
-    const to = moment(this.state.to).format(SHORT_FORMAT);
-    this.setState({ values: `Show usage from ${from} to ${to}` });
-    this.setState({
-      birth: [
-        {date: '2019-06-30', usage: rand(1500)},
-        {date: '2019-07-01', usage: rand(1500)},
-        {date: '2019-07-02', usage: rand(1500)},
-        {date: '2019-06-29', usage: rand(1500)}
-      ],
-      death: [
-        {date: '2019-06-29', usage: rand(200)},
-        {date: '2019-06-30', usage: rand(200)},
-        {date: '2019-07-01', usage: rand(200)},
-        {date: '2019-07-02', usage: rand(200)}
-      ],
-      marriage: [
-        {date: '2019-06-29', usage: rand(50)},
-        {date: '2019-06-30', usage: rand(50)},
-        {date: '2019-07-01', usage: rand(50)},
-        {date: '2019-07-02', usage: rand(50)}
-      ],
-      partnership: [
-        {date: '2019-06-29', usage: rand(24)},
-        {date: '2019-06-30', usage: rand(24)},
-        {date: '2019-07-01', usage: rand(24)},
-        {date: '2019-07-02', usage: rand(24)}
-      ],
-      dates: ['2019-06-29', '2019-06-30', '2019-07-01', '2019-07-02']
-    });
+    // e.preventDefault();
+    // const from = moment(this.state.from).format(SHORT_FORMAT);
+    // const to = moment(this.state.to).format(SHORT_FORMAT);
   }
 
   render() {
@@ -93,21 +48,8 @@ class LevReport extends React.Component {
         onSubmit={this.handleSubmit.bind(this)}
         updateFrom={this.updateFrom.bind(this)}
         updateTo={this.updateTo.bind(this)} />
-      <FullWidthMain>
-        <LevUsage
-          birth={this.state.birth}
-          death={this.state.death}
-          marriage={this.state.marriage}
-          partnership={this.state.partnership}
-          dates={this.state.dates}
-        />
-        <p>Total birth usage: {this.state.totalBirths}</p>
-        <p>Total death usage: {this.state.totalDeaths}</p>
-        <p>Total marriage usage: {this.state.totalMarriages}</p>
-        <p>Total partnership usage: {this.state.totalPartnerships}</p>
-        <p>Total across all data: {this.state.totalBirths + this.state.totalDeaths + this.state.totalMarriages + this.state.totalPartnerships}</p>
-        <pre>{JSON.stringify(this.state, 0, 2)}</pre>
-      </FullWidthMain>
+      <LevUsage dates={this.state.dates} datasets={this.state.datasets} />
+      <UsageByGroup datasets={this.state.datasets} groups={this.state.groups} totals={this.state.totals} />
     </LevPage>;
   }
 }
