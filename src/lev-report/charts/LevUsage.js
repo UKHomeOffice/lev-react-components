59224cbfc1d8fb71d4f78922572efc8d2fc27b9b
Robin Knipe <robin.knipe@gmail.com>
2019-08-22 10:59:07 +0100

    Improve usage chart labels and aesthetics
    Various imporvments
     - tilt/rotate date labels to avoid overlays
     - remove the bar labels (to replaced by a tooltip later)
     - improve the way `props` are used in the code
     - improve chart padding for better use of space
     - itterate over datasets, handle any number (not just current 4)

--- src/lev-report/charts/LevUsage.js
+++ src/lev-report/charts/LevUsage.js
@@ -5,7 +5,7 @@ import { VictoryAxis, VictoryBar, VictoryChart, VictoryLabel, VictoryStack, Vict
 const SHORT_DATE = 'D/M/YY';
 const SERVER_FORMAT = 'YYYY-MM-DD';
 
-const days = t => moment(t).format('D MMM');
+const days = t => moment(t).format('Do MMM');
 const qtr  = t => {
   const m = moment(t);
   if (m.weekday() === 1) {
@@ -15,23 +15,25 @@ const qtr  = t => {
 const year = t => {
   const m = moment(t);
   if (m.date() === 1) {
-    return m.format('MMMM');
+    return m.format(m.month() ? 'MMMM' : 'MMMM \'YY');
   }
 };
-const tickFormat = n => n < 45 ? days : n < 95 ? qtr : year;
+const tickFormat = n => n < 33 ? days : n < 95 ? qtr : year;
+
+const CustomLabel = ({ freq, ...props }) => (freq < 33) || (freq > 150) ?
+  <g transform={`rotate(-45 ${props.x} ${1.1 * props.y})`}><VictoryLabel { ...props } /></g> :
+  <VictoryLabel { ...props } />;
 
 const datatypeBar = data => data && data.length && <VictoryBar
   data={data}
   x="date"
   y="usage"
-  labels={(d) => d.usage}
-  style={{ labels: { fill: "white" } }}
   barRatio={0.9}
 /> || null;
 
-const LevUsage = props => ((props.birth && props.birth.length) || (props.death && props.death.length) ||
-  (props.marriage && props.marriage.length) || (props.partnership && props.partnership.length)) &&
+const LevUsage = ({ datasets, dates, ...props }) => datasets && datasets.length &&
 <VictoryChart
+  padding={{ top: 0, bottom: 50, left: 50, right: 0 }}
   domainPadding={20}
   theme={VictoryTheme.material}
   style={{ labels: { fontSize: 15 } }}
@@ -39,25 +41,18 @@ const LevUsage = props => ((props.birth && props.birth.length) || (props.death &
   width={600}
 >
   <VictoryAxis
-    domain={[props.dates[0], props.dates[props.dates.length - 1]]}
-    tickValues={props.dates.filter(tickFormat(props.dates.length))}
-    tickFormat={tickFormat(props.dates.length)}
+    domain={[dates[0], dates[dates.length - 1]]}
+    tickValues={dates.filter(tickFormat(dates.length))}
+    tickFormat={tickFormat(dates.length)}
+    tickLabelComponent={<CustomLabel freq={dates.length} />}
+    style={{ tickLabels: { fontSize: 10 } }}
   />
   <VictoryAxis dependentAxis />
   <VictoryStack
     colorScale="cool"
     barRatio={0.9}
-    style={{
-      labels: {
-        fontSize: 9,
-        fill: 'white',
-        padding: 15
-      }
-  }}>
-    {datatypeBar(props.birth)}
-    {datatypeBar(props.death)}
-    {datatypeBar(props.marriage)}
-    {datatypeBar(props.partnership)}
+  >
+    {datasets.map(d => datatypeBar(d.dailyUsage))}
   </VictoryStack>
 </VictoryChart> || 'No data was found';
 
