1b3964a04fa2b3dd5304e1464f72b6fc9080af6c
Robin Knipe <robin.knipe@gmail.com>
2019-11-30 14:53:25 +0000

    [LEVWEB-1340] Make dashboard UI poll for updates
    After the `DashboardComponent` _mounts_ (i.e. is hydrated), it begins
    polling for updates, via ajax, instead of requiring a page refresh.

--- src/lev-dashboard/DashboardContent.js
+++ src/lev-dashboard/DashboardContent.js
@@ -17,10 +17,37 @@ class DashboardContent extends React.Component {
     };
   }
 
+  componentDidMount() {
+    this.timerID = setInterval(() => this.updateCounter(), REFRESH);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+    this.stopAnimation();
+  }
+
+  updateCounter() {
+    const xhttp = new XMLHttpRequest();
+    const dash = this;
+    xhttp.onreadystatechange = function stateChanged() {
+      if (this.readyState === 4 && this.status === 200) {
+        const json = JSON.parse(xhttp.responseText);
+        if (dash.state.todaySearches !== json.todaySearches) {
+          dash.setState({
+            todaySearches: json.todaySearches,
+            allTime: json.allTime
+          });
+        }
+      }
+    };
+    xhttp.open('GET', 'http://localhost:4000/dashboard/data', true);
+    xhttp.send();
+  }
+
   render() {
     return <CountDiv id="dashboard">
-      <DashboardCounter period="searches today" count={this.state.todaySearches}/>
-      <DashboardCounter period="searches all time" count={this.state.allTime}/>
+      <DashboardCounter period="searches today" count={this.state.todaySearches} />
+      <DashboardCounter period="searches all time" count={this.state.allTime} />
     </CountDiv>;
   }
 }
