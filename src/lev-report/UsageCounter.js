52bf862bd9cf1fd8acb7516685fe9738bc8f1186
Aaron Gill <aaron.gill@digital.homeoffice.gov.uk>
2020-04-29 13:02:59 +0100

    [LEVWEB-1371] Counter updated with withoutGroups

--- src/lev-report/UsageCounter.js
+++ src/lev-report/UsageCounter.js
@@ -3,15 +3,15 @@ import styled from 'styled-components';
 const moment = require('moment');
 
 const StyledDiv = styled.div`
-  font-size: 40px;
-  vertical-align: text-bottom;
   display: inline-block;
+  font-size: 40px;
   margin-left: 20px;
   text-align: right;
+  overflow: auto;
   p {
     font-size: 20px;
     margin-top: 0;
-    margin-bottom: 5px;
+    margin-bottom: 3px;
   }
 `;
 
@@ -20,7 +20,8 @@ const formatDate = (date, mDate = moment(date)) => date && mDate.isValid() && mD
 
 const UsageCounter = props =>
   <StyledDiv>
-    <p>Searches {props.group ? ' for ' + props.group : ''}</p>
+    <p>Searches {props.group && 'for ' + props.group}</p>
+    {props.withoutGroups && <p>excluding selected department(s)</p>}
     <p>{(props.from ? formatDate(props.from) + ' - ' : '')}
       {(props.to ? formatDate(props.to) : 'today')}</p>
     {formatCount(props.count)}
