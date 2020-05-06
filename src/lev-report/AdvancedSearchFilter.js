a449de51ea10cf4895adfdfa8a6d0d5308113196
Aaron Gill <aaron.gill@digital.homeoffice.gov.uk>
2020-05-06 11:25:55 +0100

    [LEVWEB-1371] New components for advanced search
    ...plus count group information

---
+++ src/lev-report/AdvancedSearchFilter.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Details } from "govuk-react";
+import styled from "styled-components";
+import CheckBoxFilter from "./CheckBoxFilter";
+import CountSummary from "./CountSummary";
+
+const AdvancedSearchContainer = styled.div`
+  position: relative;
+  display: flex;
+  margin-bottom: 15px;
+`
+
+const AdvancedSearchFilter = (props) =>
+  <React.Fragment>
+    <Details summary={props.children}>
+      <AdvancedSearchContainer>
+        <CheckBoxFilter groups={props.groups} selectedGroup={props.withoutGroups} name="withoutGroup">
+          Exclude Department(s)
+        </CheckBoxFilter>
+        <CountSummary selectedGroup={props.selectedGroup} withoutGroups={props.withoutGroups} />
+      </AdvancedSearchContainer>
+    </Details>
+  </React.Fragment>
+
+export default AdvancedSearchFilter;
---
+++ src/lev-report/CountSummary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Details } from "govuk-react";
+
+const StyledDiv = styled.div`
+flex:1;
+`;
+
+const CountSummary = (props) =>
+   <StyledDiv>
+      <p>Selected Department: {props.selectedGroup ? props.selectedGroup : 'N/A'}</p>
+      <p>Excluding Department(s): {Array.isArray(props.withoutGroups) ?
+        props.withoutGroups.join(', ') : props.withoutGroups || 'N/A'}</p>
+  </StyledDiv>
+
+export default CountSummary;
+
